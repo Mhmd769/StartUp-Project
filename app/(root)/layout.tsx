fddff0b1d9d0190929e840bc02d3dad8411719ec
mhmdmahdi <mhmdmahdi76931@gmail.com>
2025-04-14 17:56:35 +0300

    Added form handling and fix an error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../../components/Navbar";
 
 export default async function Layout({ children }:Readonly<{children: React.ReactNode}>) {
   return (
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { promises } from "dns";
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({searchParams}: {
+  searchParams: Promise <{query?: string}>
+
+}) {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="heading">HOME</h1>
+    <section className="pink_container">
+    <h1 className="heading">
+      Pitch Your StartUp, <br/> Connect With Entrepreneurs
+    </h1>
+
+    <p className="sub-heading !max-w-3xl">
+      Submit Ideas , Vote On Pitches, And Get Noticed In Virtual Competitions.
+    </p>
+  
+      <SearchForm query={query} />
+
+    </section>
     </>
   );
 }
